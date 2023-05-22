2add67ecf598237c320f952f27286fc46cc7f88b
YU <133645266+YuWen2003@users.noreply.github.com>
2023-05-23 04:36:21 +0800

    Add files via upload

---
+++ Numeric_function.py
@@ -0,0 +1,12 @@
+def number(a,b,c,d):
+    if a+b+c+d>100:
+        print("Good!")
+    else:
+        print("not bad!")
+ 
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+ 
+number(a,b,c,d)
